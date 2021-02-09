c027b141d5962d2516abdd92616048f913d705d6
mflinn-broad <60187023+mflinn-broad@users.noreply.github.com>
2021-02-09 14:59:18 -0500

    Support reading parameters from a yaml config file (#5)
    * add yaml parser dependency
    
    * now supports reading from config file
    
    * parameterized previously hard coded values and read them from config file
    
    * remove extra comments

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	yaml "gopkg.in/yaml.v3"
+)
+
+// Config contains configuration values for a disk-manager run
+type Config struct {
+	TargetAnnotation string `yaml:"targetAnnotation"`
+	GoogleProject    string `yaml:"googleProject"`
+	Zone             string `yaml:"zone"`
+	Region           string `yaml:"region"`
+}
+
+func Read(configPath string) (*Config, error) {
+	configBytes, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading config file: %v", err)
+	}
+	config := new(Config)
+	if err := yaml.Unmarshal(configBytes, config); err != nil {
+		return nil, fmt.Errorf("Error parsing config: %v", err)
+	}
+	return config, nil
+}
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 
 	"github.com/broadinstitute/disk-manager/client"
+	"github.com/broadinstitute/disk-manager/config"
 	"github.com/broadinstitute/disk-manager/logs"
 	"golang.org/x/net/context"
 	"google.golang.org/api/compute/v1"
@@ -17,6 +18,10 @@ import (
 
 func main() {
 	a := parseArgs()
+	config, err := config.Read(*a.configFile)
+	if err != nil {
+		logs.Error.Fatalf("Error building config: %v", err)
+	}
 
 	logs.Info.Printf("Building clients...")
 	clients, err := client.Build(a.local, a.kubeconfig)
@@ -27,25 +32,29 @@ func main() {
 
 	// TODO - pagination needed?
 	logs.Info.Println("Searching for persistent disks...")
-	disks, err := getDisks(k8s)
+	disks, err := getDisks(k8s, config)
 	if err != nil {
 		logs.Error.Fatalf("Error retrieving persistent disks: %v\n", err)
 	}
-
-	// GCP poc starts here
 
 	ctx := context.Background()
 
 	gcp := clients.GetGCP()
 
 	logs.Info.Println("Adding snapshot policy to disks...")
-	if err := addPolicy(ctx, gcp, disks); err != nil {
+	if err := addPolicy(ctx, gcp, disks, config); err != nil {
 		logs.Error.Fatalf("Error adding snapshot policy to disks: %v\n", err)
 	}
 	logs.Info.Println("Finished updating disks, exiting...")
 }
-func getDisks(k8s *kubernetes.Clientset) ([]string, error) {
-	var disks []string
+
+type diskInfo struct {
+	name   string
+	policy string
+}
+
+func getDisks(k8s *kubernetes.Clientset, config *config.Config) ([]diskInfo, error) {
+	var disks []diskInfo
 
 	// get persistent volume claims
 	pvcs, err := k8s.CoreV1().PersistentVolumeClaims("").List(metav1.ListOptions{})
@@ -53,7 +62,7 @@ func getDisks(k8s *kubernetes.Clientset) ([]string, error) {
 		return nil, fmt.Errorf("Error retrieving persistent volume claims: %v", err)
 	}
 	for _, pvc := range pvcs.Items {
-		if _, ok := pvc.Annotations["bio.terra/snapshot-policy"]; ok {
+		if policy, ok := pvc.Annotations[config.TargetAnnotation]; ok {
 			// retrive associated persistent volume for each claim
 			pv, err := k8s.CoreV1().PersistentVolumes().Get(pvc.Spec.VolumeName, metav1.GetOptions{})
 			if err != nil {
@@ -61,43 +70,43 @@ func getDisks(k8s *kubernetes.Clientset) ([]string, error) {
 			}
 			diskName := pv.Spec.GCEPersistentDisk.PDName
 			logs.Info.Printf("found PersistentVolume: %q with disk: %q", pvc.GetName(), diskName)
-			disks = append(disks, diskName)
+			disk := diskInfo{
+				name:   diskName,
+				policy: policy,
+			}
+			disks = append(disks, disk)
 		}
 	}
 	return disks, nil
 }
 
-func addPolicy(ctx context.Context, gcp *compute.Service, disks []string) error {
-	// hardcoded params for compute api query
-	project := "broad-dsde-dev"
-	zone := "us-central1-a"
-	region := "us-central1"
-	policyName := "terra-snapshot-policy"
-
-	policy, err := gcp.ResourcePolicies.Get(project, region, policyName).Context(ctx).Do()
-	if err != nil {
-		return fmt.Errorf("Error retrieving snapshot polict: %v", err)
-	}
-	policyURL := policy.SelfLink
-
+// todo this function is doing too many things, break it up
+func addPolicy(ctx context.Context, gcp *compute.Service, disks []diskInfo, config *config.Config) error {
 	for _, disk := range disks {
-		// check to make sure disk doesn't already have a snapshot policy
-		hasPolicy, err := hasSnapshotPolicy(ctx, gcp, disk, project, zone)
+		policyName := disk.policy
+		// todo only perform this api call if policyName is different
+		policy, err := gcp.ResourcePolicies.Get(config.GoogleProject, config.Region, policyName).Context(ctx).Do()
 		if err != nil {
-			logs.Warn.Printf("unable to determine if disk %s has pre-existing resource policy, attempting to add %s", disk, policyName)
+			return fmt.Errorf("Error retrieving snapshot policy: %v", err)
+		}
+		policyURL := policy.SelfLink
+		// check to make sure disk doesn't already have a snapshot policy
+		hasPolicy, err := hasSnapshotPolicy(ctx, gcp, disk.name, config.GoogleProject, config.Zone)
+		if err != nil {
+			logs.Warn.Printf("unable to determine if disk %s has pre-existing resource policy, attempting to add %s", disk.name, policyName)
 		}
 		if hasPolicy {
-			logs.Info.Printf("disk: %s already has snapshot policy... skipping", disk)
+			logs.Info.Printf("disk: %s already has snapshot policy: %s ... skipping", disk.name, disk.policy)
 			continue
 		}
 		addPolicyRequest := &compute.DisksAddResourcePoliciesRequest{
 			ResourcePolicies: []string{policyURL},
 		}
-		_, err = gcp.Disks.AddResourcePolicies(project, zone, disk, addPolicyRequest).Context(ctx).Do()
+		_, err = gcp.Disks.AddResourcePolicies(config.GoogleProject, config.Zone, disk.name, addPolicyRequest).Context(ctx).Do()
 		if err != nil {
-			logs.Error.Printf("Error adding snapshot policy to disk %s: %v\n", disk, err)
+			logs.Error.Printf("Error adding snapshot policy: %s to disk %s: %v\n", policyName, disk.name, err)
 		}
-		logs.Info.Printf("Added snapshot policy: %s to disk: %s", policyName, disk)
+		logs.Info.Printf("Added snapshot policy: %s to disk: %s", policyName, disk.name)
 	}
 	return nil
 }
@@ -116,6 +125,7 @@ func hasSnapshotPolicy(ctx context.Context, gcp *compute.Service, disk, project,
 type args struct {
 	local      bool
 	kubeconfig *string
+	configFile *string
 }
 
 func parseArgs() *args {
@@ -126,6 +136,7 @@ func parseArgs() *args {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to kubeconfig file")
 	}
 	local := flag.Bool("local", false, "use this flag when running locally (outside of cluster to use local kube config")
+	configFile := flag.String("config-file", "/etc/disk-manger/config.yaml", "path to yaml file with disk-manager config")
 	flag.Parse()
-	return &args{*local, kubeconfig}
+	return &args{*local, kubeconfig, configFile}
 }
